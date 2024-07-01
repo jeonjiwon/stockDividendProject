10fd766789edd5b38fff3f724191e28e850c99a9
jeonjiwon <jiwon9609@naver.com>
2024-07-01 22:13:09 +0900

    init commit

---
+++ src/main/java/com/zerobase/stockDividendProject/StockDividendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.stockDividendProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDividendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockDividendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/stockDividendProject/StockDividendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.stockDividendProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDividendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
